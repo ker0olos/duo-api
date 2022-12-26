37dfa8c03d8978eb30eca3925e9b49c269fafc2c
Kerolos Zaki <ker0olos@pm.me>
2022-12-26 20:24:33 +0200

    act more as simple proxy

--- index.ts
+++ index.ts
@@ -1,55 +1,69 @@
 import { json, serve } from 'https://deno.land/x/sift@0.6.0/mod.ts';
 
-const id = Deno.env.get('USER_ID');
-const token = Deno.env.get('JWT_TOKEN');
-
+// c
 async function login(request: Request): Promise<Response> {
-  const url = 'https://www.duolingo.com/login';
+  switch (request.method) {
+    case 'POST': {
+      const url = 'https://www.duolingo.com/login';
 
-  if (request.method !== 'POST') {
-    return json(null, {
-      status: 403,
-      statusText: 'Forbidden',
-    });
+      let data = await request.json();
+
+      const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ login: data.username, password: data.password }),
+      });
+
+      data = await res.json();
+
+      if (data['failure']) {
+        return json(data, {
+          status: 403,
+          statusText: 'Forbidden',
+        });
+      }
+
+      return json({
+        id: data.user_id,
+        username: data.username,
+        token: res.headers.get('jwt'),
+      });
+    }
+
+    default:
+      return new Response('Invalid method', { status: 405 });
   }
-
-  let data = await request.json();
-
-  const res = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ login: data.username, password: data.password }),
-  });
-
-  data = await res.json();
-
-  if (data['failure']) {
-    return json(data, {
-      status: 403,
-      statusText: 'Forbidden',
-    });
-  }
-
-  return json({
-    id: data.user_id,
-    username: data.username,
-    token: res.headers.get('jwt'),
-  });
 }
 
-async function user(_: Request): Promise<Response> {
-  const url = `https://www.duolingo.com/2017-06-30/users?username=${id}`;
+async function user(request: Request): Promise<Response> {
+  switch (request.method) {
+    case 'GET': {
+      const data = new URL(request.url).searchParams;
 
-  const res = await fetch(url, {
-    headers: {
-      'Authorization': `Bearer ${token}`,
-    },
-  });
+      const id = data.get('id');
+      const token = data.get('token');
 
-  return json(await res.text());
+      if (!id || !token) {
+        return new Response('Bad request', { status: 400 });
+      }
+
+      const url = `https://www.duolingo.com/2017-06-30/users?username=${id}`;
+
+      const res = await fetch(url, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        },
+      });
+
+      return json(await res.text());
+    }
+
+    default:
+      return new Response('Invalid method', { status: 405 });
+  }
 }
 
 serve({
